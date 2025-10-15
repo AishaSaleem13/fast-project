acb11898da341ff16bc867c71709e0fd17890fcb
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-10-15 06:05:59 +0500

    last

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from .routes import authroutes
-from mangum import Mangum 
+from app.routes import authroutes
+from mangum import Mangum
 
-app= FastAPI()
+app = FastAPI()
+
 app.include_router(authroutes.router)
 
 @app.get("/")
 def root():
-     print("Root route accessed!") 
-     return {"message": "Welcome to FastAPI E-commerce!"}
+    return {"message": "Welcome to FastAPI E-commerce!"}
+
 handler = Mangum(app)
